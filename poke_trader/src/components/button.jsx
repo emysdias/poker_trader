02b7897042a7be4420d7d540499dee1e2147dccd
Emily Sousa <diassousa@yahoo.com.br>
2021-03-25 13:14:10 -0300

    add button and done introduction

--- poke_trader/src/components/button.jsx
+++ poke_trader/src/components/button.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import "../styles/layout/button.css";
 
-const Button = () => {
-  return;
-}
+const Button = ({ text, fontColor, backgroundColor }) => {
+  return (
+    <section>
+      <button
+        className="button__container"
+        style={{ color: fontColor, backgroundColor: backgroundColor }}
+      >
+        {text}
+      </button>
+    </section>
+  );
+};
 
 export default Button;
--- poke_trader/src/pages/introduction.jsx
+++ poke_trader/src/pages/introduction.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "../styles/layout/introduction.css";
 import Header from "../components/header";
+import Button from "../components/button";
 import Pokemon from "../assets/gastly.png";
 
 const Introduction = () => {
   return (
     <section>
-      <Header imgPokemon={Pokemon} color={'#392939'}/>
-      <section className="introduction__container">hello!!</section>
+      <Header imgPokemon={Pokemon} color={"#392939"} />
+      <section className="introduction__container">
+        <section className="introduction__container__image">
+          <img src={Pokemon} height="200" width="200"/>
+        </section>
+        <section className="introduction__container__text">
+          Bem vindo, este é um game baseado em pokémons e ele consiste em dois
+          jogadores ofertarem entre 1 e 6 pokémons de cada lado. Qualquer
+          combinação é válida. Por exemplo, trocar 2 pokémons iguais por 4
+          distintos.
+        </section>
+        <section className="introduction__container__button">
+          <Button
+            fontColor={"#ffffff"}
+            backgroundColor={"#DE5239"}
+            text={"Começar"}
+          />
+        </section>
+      </section>
     </section>
   );
 };
