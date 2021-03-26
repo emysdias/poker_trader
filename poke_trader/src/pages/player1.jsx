75a712b20454ccecdc40dd5901afee9e153a9a90
Emily Sousa <diassousa@yahoo.com.br>
2021-03-25 22:49:50 -0300

    consuming api of pokemon

--- poke_trader/src/pages/player1.jsx
+++ poke_trader/src/pages/player1.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import api from "../services/api";
 import "../styles/layout/player1.css";
 
 import Header from "../components/header";
@@ -8,8 +8,43 @@ import Button from "../components/button";
 import Pokemon1 from "../assets/bulbasaur.png";
 
 const Player1 = () => {
+  const [pokemon, setPokemon] = useState([]);
   const green = "#184A4A";
   const lightGreen = "#A4D541";
+
+  useEffect(() => {
+    const loadPokemons = async () => {
+      try {
+        var numbers = [];
+        setPokemon([]);
+
+        for (var i = 0; i < 6; i++) {
+          const value = Math.random();
+          const multiplied = value * 500;
+          const answer = Math.floor(multiplied);
+          if (answer > 0) {
+            numbers.push(answer);
+          } else if (answer < 0) {
+            i--;
+          }
+        }
+
+        numbers.map(async (index) => {
+          const response = await api.get(`/v2/pokemon/${index}`);
+          setPokemon((pokemon) => [
+            ...pokemon,
+            {
+              name: response.data.name,
+              experience: response.data.base_experience,
+            },
+          ]);
+        });
+      } catch (error) {
+        alert("Ocorreu um erro!");
+      }
+    };
+    loadPokemons();
+  }, []);
 
   return (
     <section className="player1__container">
@@ -22,18 +57,23 @@ const Player1 = () => {
         />
       </section>
       <section className="player1__container__content">
-        <section className="player1__container__content__select">
-          <section className="player1__container__content__select__name">
-            name1
-          </section>
-          <input
-            className="player1__container__content__select__input"
-            type="number"
-            min="0"
-            max="6"
-          />
-        </section>
-        <hr style={{ borderColor: lightGreen }} />
+        {pokemon &&
+          pokemon.map((item, index) => (
+            <section key={index}>
+              <section className="player1__container__content__select">
+                <section className="player1__container__content__select__name">
+                  {item.name}
+                </section>
+                <input
+                  className="player1__container__content__select__input"
+                  type="number"
+                  min="0"
+                  max="6"
+                />
+              </section>
+              <hr style={{ borderColor: lightGreen }} />
+            </section>
+          ))}
       </section>
       <section className="player1__container__button">
         <Link style={{ textDecoration: "none" }} to={{ pathname: "/second" }}>
--- poke_trader/src/pages/player2.jsx
+++ poke_trader/src/pages/player2.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import api from "../services/api";
 
 import "../styles/layout/player2.css";
 
@@ -8,9 +9,44 @@ import Button from "../components/button";
 import Pokemon from "../assets/mrmime.png";
 
 const Player2 = () => {
+  const [pokemon, setPokemon] = useState([]);
   const purple = "#628BBD";
   const white = "#FFFFFF";
   const pink = "#FF5A6A";
+
+  useEffect(() => {
+    const loadPokemons = async () => {
+      try {
+        var numbers = [];
+        setPokemon([]);
+
+        for (var i = 0; i < 6; i++) {
+          const value = Math.random();
+          const multiplied = value * 500;
+          const answer = Math.floor(multiplied);
+          if (answer > 0) {
+            numbers.push(answer);
+          } else if (answer < 0) {
+            i--;
+          }
+        }
+
+        numbers.map(async (index) => {
+          const response = await api.get(`/v2/pokemon/${index}`);
+          setPokemon((pokemon) => [
+            ...pokemon,
+            {
+              name: response.data.name,
+              experience: response.data.base_experience,
+            },
+          ]);
+        });
+      } catch (error) {
+        alert("Ocorreu um erro!");
+      }
+    };
+    loadPokemons();
+  }, []);
 
   return (
     <section className="player2__container">
@@ -19,18 +55,23 @@ const Player2 = () => {
         <Button fontColor={white} backgroundColor={pink} text={"Jogador 2"} />
       </section>
       <section className="player2__container__content">
-        <section className="player2__container__content__select">
-          <section className="player2__container__content__select__name">
-            name1
-          </section>
-          <input
-            className="player2__container__content__select__input"
-            type="number"
-            min="0"
-            max="6"
-          />
-        </section>
-        <hr style={{ borderColor: pink }} />
+        {pokemon &&
+          pokemon.map((item, index) => (
+            <section key={index}>
+              <section className="player1__container__content__select">
+                <section className="player1__container__content__select__name">
+                  {item.name}
+                </section>
+                <input
+                  className="player1__container__content__select__input"
+                  type="number"
+                  min="0"
+                  max="6"
+                />
+              </section>
+              <hr style={{ borderColor: pink }} />
+            </section>
+          ))}
       </section>
       <section className="player2__container__button">
         <Link style={{ textDecoration: "none" }} to={{ pathname: "/result" }}>
